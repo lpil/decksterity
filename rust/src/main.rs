24b8f4fe78a55b21d5fdd4f0a16c7530b7708e9a
Louis Pilfold <louis@lpil.uk>
2018-03-02 21:34:44 +0000

    DSP chain

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,28 @@
 extern crate cpal;
+extern crate dsp;
+
+/// SoundStream is currently generic over i8, i32 and f32. Feel free to change it!
+type Output = f32;
+
+type Phase = f64;
+type Frequency = f64;
+type Volume = f32;
+
+const CHANNELS: usize = 2;
+const SAMPLE_HZ: f64 = 44_100.0;
+
+const A5_HZ: Frequency = 440.0;
+const D5_HZ: Frequency = 587.33;
+const F5_HZ: Frequency = 698.46;
+
+use dsp::{Frame, FromSample, Graph, Node, Sample};
+use dsp::sample::ToFrameSliceMut;
 
 fn main() {
+    //
+    // Construct audio output
+    //
+
     let device = cpal::default_output_device().expect("Failed to get default output device");
     let format = device
         .default_output_format()
@@ -9,43 +31,72 @@ fn main() {
     let stream_id = event_loop.build_output_stream(&device, &format).unwrap();
     event_loop.play_stream(stream_id.clone());
 
-    let sample_rate = format.sample_rate.0 as f32;
-    let mut sample_clock = 0f32;
+    //
+    // Construct our dsp graph.
+    //
 
-    // Produce a sinusoid of maximum amplitude.
-    let mut next_value = || {
-        sample_clock = (sample_clock + 1.0) % sample_rate;
-        (sample_clock * 440.0 * 2.0 * 3.141592 / sample_rate).sin()
-    };
+    let mut graph = Graph::new();
 
+    // Construct our fancy Synth and add it to the graph!
+    let synth = graph.add_node(DspNode::Synth);
+
+    // Connect a few oscillators to the synth.
+    graph.add_input(DspNode::Oscillator(0.0, A5_HZ, 0.2), synth);
+    graph.add_input(DspNode::Oscillator(0.0, D5_HZ, 0.1), synth);
+    graph.add_input(DspNode::Oscillator(0.0, F5_HZ, 0.15), synth);
+
+    // Set the synth as the master node for the graph.
+    graph.set_master(Some(synth));
+
+    // The callback we'll use to pass to the Stream. It will request audio from our dsp_graph.
     event_loop.run(move |_, data| {
-        if let cpal::StreamData::Output { buffer: b } = data {
-            match b {
-                cpal::UnknownTypeOutputBuffer::U16(mut buffer) => {
-                    for sample in buffer.chunks_mut(format.channels as usize) {
-                        let value = ((next_value() * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
-                        for out in sample.iter_mut() {
-                            *out = value;
-                        }
-                    }
-                }
-                cpal::UnknownTypeOutputBuffer::I16(mut buffer) => {
-                    for sample in buffer.chunks_mut(format.channels as usize) {
-                        let value = (next_value() * std::i16::MAX as f32) as i16;
-                        for out in sample.iter_mut() {
-                            *out = value;
-                        }
-                    }
-                }
-                cpal::UnknownTypeOutputBuffer::F32(mut buffer) => {
-                    for sample in buffer.chunks_mut(format.channels as usize) {
-                        let value = next_value();
-                        for out in sample.iter_mut() {
-                            *out = value;
-                        }
-                    }
-                }
+        let b = match data {
+            cpal::StreamData::Output { buffer: b } => b,
+            _ => return,
+        };
+
+        match b {
+            cpal::UnknownTypeOutputBuffer::F32(mut buffer) => {
+                let raw_buffer: &mut [[Output; CHANNELS]] = buffer.to_frame_slice_mut().unwrap();
+                dsp::slice::equilibrium(raw_buffer);
+                graph.audio_requested(raw_buffer, SAMPLE_HZ);
             }
+            _ => return,
         }
     });
+}
+
+/// Our type for which we will implement the `Dsp` trait.
+#[derive(Debug)]
+enum DspNode {
+    /// Synth will be our demonstration of a master GraphNode.
+    Synth,
+    /// Oscillator will be our generator type of node, meaning that we will override
+    /// the way it provides audio via its `audio_requested` method.
+    Oscillator(Phase, Frequency, Volume),
+}
+
+impl Node<[Output; CHANNELS]> for DspNode {
+    /// Here we'll override the audio_requested method and generate a sine wave.
+    fn audio_requested(&mut self, buffer: &mut [[Output; CHANNELS]], sample_hz: f64) {
+        match *self {
+            DspNode::Synth => (),
+            DspNode::Oscillator(ref mut phase, frequency, volume) => {
+                dsp::slice::map_in_place(buffer, |_| {
+                    let val = sine_wave(*phase, volume);
+                    *phase += frequency / sample_hz;
+                    Frame::from_fn(|_| val)
+                });
+            }
+        }
+    }
+}
+
+/// Return a sine wave for the given phase.
+fn sine_wave<S: Sample>(phase: Phase, volume: Volume) -> S
+where
+    S: Sample + FromSample<f32>,
+{
+    use std::f64::consts::PI;
+    ((phase * PI * 2.0).sin() as f32 * volume).to_sample::<S>()
 }
